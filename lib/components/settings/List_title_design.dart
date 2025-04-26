3795930f9d0781a540fe7aa7714a1cbfc92e1392
GADO <hossammahmoudgado@gmail.com>
2025-04-26 16:34:22 +0300

    settings finish

---
+++ lib/components/settings/List_title_design.dart
@@ -0,0 +1,65 @@
+import 'package:doctor_appointment/models/settings_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ListTitleDesign extends StatelessWidget {
+  const ListTitleDesign({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SettingsModel(
+            onTap: (){},
+            containerColor: Color(0xffbeddfc),
+            color: Colors.blue,
+            title: "Profile",
+            leadingIcon: CupertinoIcons.person,
+            icon: Icons.arrow_forward_ios),
+        SizedBox(height: 20,),
+        SettingsModel(
+            onTap: (){
+            },
+            containerColor: Color(0xffd3c3e8),
+            color: Color(0xff673db6),
+            title: "Notification",
+            leadingIcon: Icons.notifications_none,
+            icon: Icons.arrow_forward_ios),
+        SizedBox(height: 20,),
+        SettingsModel(
+            onTap: (){
+            },
+            containerColor: Color(0xffc3cbe7),
+            color: Color(0xff7e88d2),
+            title: "Privacy",
+            leadingIcon: Icons.privacy_tip_outlined,
+            icon: Icons.arrow_forward_ios),
+        SizedBox(height: 20,),
+        SettingsModel(
+            onTap: (){},
+            containerColor: Color(0xffc9e6c8),
+            color: Colors.green,
+            title: "General",
+            leadingIcon: Icons.settings_suggest_outlined,
+            icon: Icons.arrow_forward_ios),
+        SizedBox(height: 20,),
+        SettingsModel(
+            onTap: (){},
+            containerColor: Color(0xfffddeb3),
+            color: Colors.orange,
+            title: "About us",
+            leadingIcon: Icons.info_outline,
+            icon: Icons.arrow_forward_ios),
+        SizedBox(height: 20,),
+        Divider(height: 50,),
+        SettingsModel(
+            onTap: (){},
+            containerColor: Color(0xffe87e74),
+            color: Color(0xffdb4d4b),
+            title: "Log Out",
+            leadingIcon: Icons.logout,
+            icon: Icons.arrow_forward_ios),
+      ],
+    );
+  }
+}
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,12 +1,48 @@
+import 'package:doctor_appointment/components/settings/List_title_design.dart';
 import 'package:flutter/material.dart';
-
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepPurpleAccent,
+    return Padding(
+      padding: const EdgeInsets.only(top: 60, left: 20, right: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Settings",
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ListTile(
+            leading: Container(
+              width: 60,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/doctor1.jpg"),
+                ),
+              ),
+            ),
+            title: const Text(
+              "Dr.Doctor Name",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            subtitle: const Text(
+              "Profile",
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          const Divider(
+            height: 50,
+            thickness: 0.5,
+          ),
+          const ListTitleDesign(),
+        ],
+      ),
     );
   }
 }
